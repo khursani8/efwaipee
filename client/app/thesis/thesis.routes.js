2433f3de179a428523d356e5384aed839caae33f
sani <khursani8@gmail.com>
2017-08-03 23:35:37 +0800

    fix material_select

--- client/app/thesis/thesis.routes.js
+++ client/app/thesis/thesis.routes.js
@@ -5,11 +5,21 @@ export default function($stateProvider) {
   $stateProvider
     .state('thesis', {
       url: '/thesis',
-      template: '<thesis></thesis>'
+      template: '<thesis></thesis>',
+      onEnter:function () {
+                   setTimeout(()=>{
+                        console.log( "window loaded dlm onenter" );
+                        $('.tooltipped').tooltip({delay: 50});
+                        $('select').material_select();
+                        $('.modal').modal();
+                   },1000)
+
+                }
     })
     .state('thesis.details',{
       url:'/:id',
       template: '<details open></details>'
-    })
+    }
+  )
     ;
 }
